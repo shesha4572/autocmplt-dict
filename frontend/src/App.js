6f5d61b37de3ce364f7ed2e8039063d4ef952547
Rahul-2704 <rahulruke35@gmail.com>
2022-10-23 21:26:53 +0530

    test commit from rahul

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
             <Route exact path = "/" element = {<Form/>}/>
             <Route path = "/:getMeaning" element = {<PrivateRoute> <Dictionary/></PrivateRoute>}/>
         </Routes>
+
     )
 }
 
